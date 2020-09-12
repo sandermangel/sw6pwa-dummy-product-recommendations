086b03cd78df1b048e1570723599321ea087b048
Sander Mangel <sander@sandermangel.nl>
2020-09-12 08:03:56 +0200

    define data explicitly

--- useRecommendations.js
+++ useRecommendations.js
@@ -2,7 +2,16 @@ import axios from 'axios';
 
 export default {
     async getRecommendations () {
-        const response = await axios.get('https://fakestoreapi.com/products');
-        return response.data.slice(0, 5);
+        let response = await axios.get('https://fakestoreapi.com/products');
+        return response.data.slice(0, 5).map((item) => {
+            return {
+                id: item.id,
+                title: item.title,
+                image: item.image,
+                price: '€' + item.price,
+                specialPrice: null,
+                rating: Math.floor(Math.random() * 3) + 1
+            }
+        })
     }
 }
